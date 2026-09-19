0af7f048209b5925700b0b4caa589f29c1cdfa0c
view <viuu@odds.team>
2026-09-19 23:54:24 +0700

    add dim and fct in dbt

---
+++ dbt/models/marts/dim_device_headphones.sql
@@ -0,0 +1,13 @@
+with stg as (
+    select * from {{ ref('stg_audiophile') }}
+    where device_type = 'headphones'
+)
+
+select distinct
+    cityHash64(device_type, model) as device_id,
+    model,
+    signature,
+    driver_type,
+    fit_cup,
+    based_on
+from stg
